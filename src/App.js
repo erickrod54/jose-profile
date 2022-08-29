884d6206905bea31974f8e1c504f5560a2901069
Erick Rodriguez <erickrod54@gmail.com>
2022-08-29 01:09:27 -0400

    Jose-profile web - version 1 - App js - Features: Setting up react-router version 6, Importing and Placing pages and Components - next versions ill fix the routing to the right components-

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from "react";
+
+/**Routing */
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
+/**Components and pages */
 import NavBar from "./components/navbar.component";
 import HomePage from "./pages/home.page";
 import ErrorPage from "./pages/error.page";
 import Footer from "./components/footer.component";
+
+/**Jose-profile web - version 1 - App js - Features:
+ * 
+ *      --> Setting up 'react-router' version 6
+ * 
+ *      --> Importing and Placing pages and 
+ *          Components.
+ * 
+ * Notes: For now all routes are targetting to
+ * home, but eventually in next version i'll
+ * fix it routing to each route matching the
+ * right Component-Page
+*/
 
 
 function App() {
@@ -13,6 +29,10 @@ function App() {
       <NavBar />
         <Routes>
           <Route path="/" element={<HomePage />}/>
+          <Route path="/intro" element={<HomePage />}/>
+          <Route path="/work-experience" element={<HomePage />}/>
+          <Route path="/skills" element={<HomePage />}/>
+          <Route path="/languages" element={<HomePage />}/>
           <Route path="*" element={<ErrorPage />}/>
         </Routes>
       <Footer />
