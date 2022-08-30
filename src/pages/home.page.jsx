b253115cad67f4a6aa597e10e38b0f670ffd8c30
Erick Rodriguez <erickrod54@gmail.com>
2022-08-30 00:16:37 -0400

    Jose-profile web - version 1 - HomePage - Features: Importing and Placing Sections and Content components in order to display the data

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -1,13 +1,45 @@
 import React from "react";
+import styled from "styled-components";
 
+import Sections from "../components/sections.component";
+import Content from "../components/content.component";
+
+/**Jose-profile web - version 1 - HomePage - Features:
+ * 
+ *      --> Importing and Placing 'Sections' and 
+ *         'Content'  components in order to
+ *          display the data
+ * 
+ * Notes: For now all routes are targetting to
+ * home, but eventually in next version i'll
+ * fix it routing to each route matching the
+ * right Component-Page
+*/
 
 const HomePage = () => {
 
     return(
-    <div className="page-100">
-        <h2>Home Page</h2>
-    </div>
+    <div>
+    <Wrapper>
+        <Sections className='sections'/>
+        <Content  className='content'/>
+    </Wrapper>
+    </div> 
     )
 }
+
+const Wrapper = styled.section`
+  display: flex;
+  flex-direction: row;
+
+  .sections{
+    margin: 0 auto;
+    border-radius: 2px solid black;
+  }
+
+  .content{
+    margin: 0 auto;
+  }
+`
 
 export default HomePage;
