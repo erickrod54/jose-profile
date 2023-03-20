7e77695c7727d00226560c0a845c633491ce2fc8
Erick Rodriguez <erickrod54@gmail.com>
2023-03-19 22:08:01 -0400

    Jose-profile web - version 2.01 - index components js - Features: Building the index and importing and exporting all HomePage Components, Changing versioning to two digits

---
+++ src/components/index.components.js
@@ -0,0 +1,20 @@
+import IntroComponent from "./intro.component";
+import NavBar from "./navbar.component";
+import Footer from "./footer.component";
+import WorkComponent from "./work.component";
+import SkillsComponent from "./skills.component";
+import LanguagesComponent from "./languages.component";
+
+/**Jose-profile web - version 2.01 - index components js - 
+ * Features:
+ * 
+ *      --> Building the index and importing and 
+ *          exporting all HomePage Components.
+ * 
+ *      --> Changing versioning to two digits.
+ * 
+ * Notes: Every component will be uncommented as it is
+ * finish
+*/
+
+export { NavBar, IntroComponent, Footer, WorkComponent, SkillsComponent, LanguagesComponent }
