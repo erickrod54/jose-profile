f7fea460f851e4a7eaadfd0024e7b5e9c367dcdf
Erick Rodriguez <erickrod54@gmail.com>
2023-03-23 02:42:51 -0400

    jose-profile web - version 2.02 - FooterComponent - Features: Clearing code from old Markup, Refactoring the app

--- src/components/footer.component.jsx
+++ src/components/footer.component.jsx
@@ -1,104 +1,41 @@
 import React from "react";
-import styled from "styled-components";
-import { BsGithub } from 'react-icons/bs'
-import QRComponent from "./vcard.component";
+import { useJoseProfileContext } from "../context";
 import QRDev from "./erick.vcard.component";
 
-/**Jose-profile web - version 2 - Footer 
- * Component - Features:
+/**Jose-profile web - version 2.02 - FooterComponent - Features:
  * 
- *      --> Importing and placing
- *         'QRComponent' my contact.
+ *      --> Clearing code from old Markup.
  * 
- *      --> Importing and placing 'QRDev'
+ *      --> Refactoring the app.
  * 
- *      --> Re-styling  'Wrapper'
- * 
- * Notes: By this version is set in this 
- * Component.
+ * Notes: All these are clean files in data, directory and 
+ * files
 */
 
-const Footer = () => {
+const FooterComponent = () => {
 
-    return(
-    <Wrapper>
-        <ul>
-            <li>
-            <QRComponent />
-            </li>
-            <li>
-            <h5>&copy; { new Date().getFullYear()}</h5>
-            <span> JP  |  Data Analyst Cognizant  </span>
-            <h5>  All Rights reserved</h5>    
-            </li>
-            <li className="third">
-            <span>Developed by  @erickrod54</span>
-            <div className="third-icons">
-            <a href="https://github.com/erickrod54">
-            <BsGithub />
-            </a>
-            <QRDev />    
+    const { GitHubIcon } = useJoseProfileContext()
+
+    return (
+        <section id='footer'>
+            <div>
+                    <a href="https://github.com/erickrod54">
+                      {React.cloneElement(GitHubIcon, {style: {height: '5rem', width: '5rem'}})}
+                    </a>
             </div>
-            </li>
-        </ul>     
-    </Wrapper>
+            <div>
+                <h5>&copy; { new Date().getFullYear()}</h5>
+                <span> JP  |  Data Analyst Cognizant  </span>
+                <h5>  All Rights reserved</h5>
+            </div>
+            <div>
+                <span>Developed by  @erickrod54</span>
+                <div className="third-icons">
+                    <QRDev />
+                </div>
+            </div>
+        </section>
     )
 }
 
-
-const Wrapper = styled.footer`
-    background-color: var(--clr-primary-15);
-    padding: 1rem;
-    color: var(--clr-white);
-
-    ul{
-        display: flex;
-        justify-content: space-around;
-    }
-
-    .third{
-        display: flex;
-        flex-flow: column;
-    }
-    .third-icons{
-        margin-top: 1rem;
-        padding: .5rem;
-        display: flex;
-    }
-
-    svg{
-        width: 4rem;
-        height: 4rem;
-    }
-
-    svg:hover{
-        background-color: var(--clr-primary-12);
-        border-radius: 50%;
-    }
-`
-
-export default Footer;
-
-/**
- * 
- * <ul>
-            <li>
-            <QRComponent />
-            </li>
-            <li>
-            <span>Developed by  @erickrod54</span>
-            <a href="https://github.com/erickrod54">
-                <BsGithub />
-            </a>
-            </li>
-            <li>
-            <QRDev />    
-            </li>
-        </ul>
-
-        <div>
-            <h5>&copy; { new Date().getFullYear()}</h5>
-            <span> JP  |  Data Analyst Cognizant  </span>
-            <h5>  All Rights reserved</h5>
-        </div>
- */
+export default FooterComponent;
