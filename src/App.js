087950aac8709362dc50ec4628d9c6567016bf42
Erick Rodriguez <erickrod54@gmail.com>
2023-03-23 02:39:14 -0400

    jose-profile web - version 2.02 - App js - Features: Clearing components and routing, Refactoring the app

--- src/App.js
+++ src/App.js
@@ -1,35 +1,19 @@
 import React from "react";
-
-/**Routing */
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-
-/**Components and pages */
 import HomePage from "./pages/home.page";
-import ErrorPage from "./pages/error.page";
 
-/**Jose-profile web - version 2.01 - App js - Features:
+/**Jose-profile web - version 2.02 - App js - Features:
  * 
- *      --> Migrating 'NavBar' and 'Footer' Component.
+ *      --> Clearing components and routing
  * 
- *      --> Changing versioning to two digits.
+ *      --> Refactoring the app.
  * 
- * Notes: This Component can have later changes respecting
- * to a Shared Layout
+ * Notes: All these are clean files in data, directory and 
+ * files
 */
-
 
 function App() {
   return (
-    <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />}/>
-          <Route path="/intro" element={<HomePage />}/>
-          <Route path="/work-experience" element={<HomePage />}/>
-          <Route path="/skills" element={<HomePage />}/>
-          <Route path="/languages" element={<HomePage />}/>
-          <Route path="*" element={<ErrorPage />}/>
-        </Routes>
-    </Router>
+    <HomePage/>
   );
 }
 
