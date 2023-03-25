f3df36db9a72425a92ff7bda94ece39a14fded0e
Erick Rodriguez <erickrod54@gmail.com>
2023-03-24 21:37:32 -0400

    Jose-profile web - version 2.02 - index js - Features: Refactoring the App js

--- src/index.js
+++ src/index.js
@@ -2,33 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { AppProvider  } from './context';
-import { Helmet } from 'react-helmet'
+import { JoseProfileProvider } from './context';
+import { Helmet } from 'react-helmet';
 
-/**jose-profile page - version 2 - index js - 
- * Features: 
+/**Jose-profile web - version 2.02 - index js - Features:
  * 
- *      --> Implementing 'Helmet' to change title and
- *          set some description for SEO. 
+ *      --> Refactoring the app.
  * 
- * Notes: By this version i'll work on styling the app 
- * and add essential components
- * */
+ * Notes: All these are clean files in data, directory and 
+ * files
+*/
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-  <Helmet>
+    <Helmet>
       <meta charSet="utf-8" />
       <title>Jose A. Pahmer - Web Profile</title>
       <link rel="canonical" href="https://joseantonioxv.netlify.app/" />
       <meta name="description" content="Jose A. Pahmer V. web profile, job background and projects"></meta>
-  </Helmet>
-  <React.StrictMode>
-   <AppProvider>
-      <App />
-   </AppProvider>
-  </React.StrictMode>
+    </Helmet>
+    <React.StrictMode>
+      <JoseProfileProvider>
+        <App />
+      </JoseProfileProvider>
+    </React.StrictMode>
   </>
 );
-
