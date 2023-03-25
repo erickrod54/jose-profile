1155668324ed1f26c3567079016852d11600fbce
Erick Rodriguez <erickrod54@gmail.com>
2023-03-25 15:57:08 -0400

    Jose-profile web - version 2.02 - index.assets js - Features: Clearing assets and Refactoring hte app

---
+++ src/assets/index.assets.js
@@ -0,0 +1,13 @@
+import ErickQR from './erick-qr-code.svg';
+
+/**Jose-profile web - version 2.02 - index.assets js - Features:
+ * 
+ *      --> Clearing assets
+ * 
+ *      --> Refactoring the app.
+ * 
+ * Notes: All these are clean files in data, directory and 
+ * files
+*/
+
+export { ErickQR }
