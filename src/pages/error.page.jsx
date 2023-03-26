afe86ceb7307058a7f91529469592029b846c15c
Erick Rodriguez <erickrod54@gmail.com>
2023-03-26 13:55:01 -0400

    Jose-profile web - version 2.04 - ErrorPage Component - Features: Placing button to navigate back to HomePage

--- src/pages/error.page.jsx
+++ src/pages/error.page.jsx
@@ -1,17 +1,26 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
 
-/**Jose-profile web - version 2.03 - Skills Component - Features:
+/**Jose-profile web - version 2.04 - ErrorPage Component - Features:
  * 
- *      --> Keeping 'ErrorPage'.
+ *      --> Placing button to 'navigate' back to 'HomePage'.
  * 
  * Notes: Pending to set navigation and styles for 'ErrorPage'
 */
 
 const ErrorPage = () => {
 
+    const navigate = useNavigate()
+
+    const handleHome = () => {
+
+        navigate('/')
+    }
+
     return(
     <div className="page-100">
         <h2>Error Page</h2>
+        <button onClick={handleHome}> Go Back Home Page</button>
     </div>
     )
 }
