5289397cdcf10f8ca8b5a471f8feaea2aa4cb6dd
Erick Rodriguez <erickrod54@gmail.com>
2023-03-26 13:42:10 -0400

    Jose-profile web - version 2.03 - App js - Features: Keeping Error Page - Pending to set navigations and styles

--- src/pages/error.page.jsx
+++ src/pages/error.page.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
+/**Jose-profile web - version 2.03 - Skills Component - Features:
+ * 
+ *      --> Keeping 'ErrorPage'.
+ * 
+ * Notes: Pending to set navigation and styles for 'ErrorPage'
+*/
+
 const ErrorPage = () => {
 
     return(
