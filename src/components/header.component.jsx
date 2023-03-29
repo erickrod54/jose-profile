2db05cb375a6ffb9314cab1f361b88818ab8bdde
Erick Rodriguez <erickrod54@gmail.com>
2023-03-28 21:30:10 -0400

    Jose-profile web - version 2.15 - index.css - Features: Wrapping HeaderComponent with stytle Component HeaderWrapper

--- src/components/header.component.jsx
+++ src/components/header.component.jsx
@@ -1,31 +1,50 @@
 import React from "react";
+import styled from "styled-components";
 import { useJoseProfileContext } from "../context";
 
-/**Jose-profile web - version 2.07 - HeaderComponent - Features:
+
+/**Jose-profile web - version 2.15 - HeaderComponent - Features:
  * 
- *      --> Wrapping title and logo using 'header--container'.
+ *      --> Wrapping 'HeaderComponent' with style 
+ *         'HeaderWrapper'.
  * 
  * Notes: All these are clean files in data, directory and 
  * files
-*/
+ */
 
 const HeaderComponent = () => {
 
     const { headerData } = useJoseProfileContext();
 
     console.log('headerData ==>', headerData)
-
+    
     const logo = headerData[0].sectionname;
     const welcomemsg = headerData[0].text;
 
     return(
-        <section id='header'>
+        <HeaderWrapper id='header'>
             <div className="header--container">
                 <h2 className="header--title">{logo}</h2>
                 <p className="header--logo">{welcomemsg}</p>
             </div>
-        </section>
+        </HeaderWrapper>
     )
 }
+
+const HeaderWrapper = styled.section`
+    
+    .header--container{
+        display: grid;
+        padding: 1.5rem;
+        margin-left: 1rem;
+    }
+
+    .header--title {
+        color: var(--clr-white);
+        font-weight: 500;
+        font-size: var(--font-size-title);
+        width: fit-content;
+    }
+`
 
 export default HeaderComponent;
