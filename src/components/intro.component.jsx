7627d609071e75efffa0aefd12928d6730e6731f
Erick Rodriguez <erickrod54@gmail.com>
2024-12-22 21:30:00 -0500

    Jose-profile-web version 2.18 - IntroComponent - Features: writting style component

--- src/components/intro.component.jsx
+++ src/components/intro.component.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import { useJoseProfileContext } from "../context";
+import styled from "styled-components";
 
-/**Jose-profile web - version 2.02 - IntroComponent - Features:
+/**Jose-profile web - version 2.18 - IntroComponent - Features:
  * 
- *      --> Clearing Code from old Markup.
- * 
- *      --> Refactoring the app.
+ *      --> Wrrritng 'IntroContainer' style component
  * 
  * Notes: All these are clean files in data, directory and 
  * files
 */
+
+const IntroContainer = styled.section`
+    display: grid;
+    width: 30rem;
+    height: fit-content;
+    grid-column: center-start / col-end 3;
+    grid-template-rows: repeat(2);
+    background: white;
+`
 
 const IntroComponent = () => {
 
@@ -20,12 +28,12 @@ const IntroComponent = () => {
     const description = intro[0].text;
 
     return(
-        <section id='intro'>
+        <IntroContainer>
             <h2 className="intro--title">{introtitle}</h2>
             <p className="intro--para">
                 {description}
             </p>
-        </section>
+        </IntroContainer>
     )
 }
 
