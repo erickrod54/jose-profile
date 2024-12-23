325ccd8c85e4628ce3bc15f3f02b952d2a8258eb
Erick Rodriguez <erickrod54@gmail.com>
2024-12-22 21:31:00 -0500

    Jose-profile web - version 2.19 - style.components - Features: writting IntroContainer styles

---
+++ src/style.components.js
@@ -0,0 +1,18 @@
+import styled from "styled-components"
+
+/**Jose-profile web - version 2.19 - style.components - Features:
+ * 
+ *      --> writting 'IntroContainer' styles
+ * 
+ * Notes: All these are clean files in data, directory and 
+ * files
+ */
+
+export const IntroContainer = styled.section`
+    display: grid;
+    width: 30rem;
+    height: fit-content;
+    grid-column: center-start / col-end 3;
+    grid-template-rows: repeat(2);
+    background: white;
+`
