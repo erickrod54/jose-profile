11c9c574a77a82717a29a8eba13cd8cf97b68cd5
Erick Rodriguez <erickrod54@gmail.com>
2024-12-22 21:32:00 -0500

    Jose-profile web - version 2.19 - style components > index - Features: Importing and exporting IntroContainer

---
+++ src/style.components.index.js
@@ -0,0 +1,12 @@
+import { IntroContainer } from "./style.components";
+
+/**Jose-profile web - version 2.19 - style components > index - 
+ * Features:
+ * 
+ *      --> Importing ajnd exporting 'IntroContainer'.
+ * 
+ * Notes: All these are clean files in data, directory and 
+ * files
+ */
+
+export { IntroContainer }
