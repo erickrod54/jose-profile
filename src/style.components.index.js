f44a163da6e2bf15191c8f90528f86f31b713e74
Erick Rodriguez <erickrod54@gmail.com>
2024-12-25 21:35:00 -0500

    Jose-profile web - version 3.09 - styles components > index - Features: Importing and exporting LanguageListItem

--- src/style.components.index.js
+++ src/style.components.index.js
@@ -1,12 +1,12 @@
-import { DescriptionWrapper, IntroContainer, IntroTitle, LanguageList, LanguagesWrapper } from "./style.components";
+import { DescriptionWrapper, IntroContainer, IntroTitle, LanguageList, LanguageListItem, LanguagesWrapper } from "./style.components";
 
-/**Jose-profile web - version 3.08 - style components > index - 
+/**Jose-profile web - version 3.09 - style components > index - 
  * Features:
  * 
- *      --> Importing and exporting 'LanguageList'.
+ *      --> Importing and exporting 'LanguageListItem'.
  * 
  * Notes: All these are clean files in data, directory and 
  * files
  */
 
-export { IntroContainer, IntroTitle, LanguagesWrapper,  DescriptionWrapper, LanguageList }
+export { IntroContainer, IntroTitle, LanguagesWrapper,  DescriptionWrapper, LanguageList, LanguageListItem }
