99d7baf935fc40ec05da01e42b210c811cc80053
Erick Rodriguez <erickrod54@gmail.com>
2024-12-27 21:31:00 -0500

    Jose-profikle web - version 3.12 - styles components > index - Features: importing and exporting LanguagesSpan

--- src/style.components.index.js
+++ src/style.components.index.js
@@ -1,12 +1,12 @@
-import { DescriptionWrapper, IntroContainer, IntroTitle, LanguageList, LanguageListItem, LanguagesWrapper, ListItem } from "./style.components";
+import { DescriptionWrapper, IntroContainer, IntroTitle, LanguageList, LanguageListItem, LanguagesSpan, LanguagesWrapper, ListItem } from "./style.components";
 
-/**Jose-profile web - version 3.11 - style components > index - 
+/**Jose-profile web - version 3.12 - style components > index - 
  * Features:
  * 
- *      --> Importing and exporting 'ListItem'.
+ *      --> Importing and exporting 'LanguagesSpan'.
  * 
  * Notes: All these are clean files in data, directory and 
  * files
  */
 
-export { IntroContainer, IntroTitle, LanguagesWrapper,  DescriptionWrapper, LanguageList, LanguageListItem, ListItem }
+export { IntroContainer, IntroTitle, LanguagesWrapper,  DescriptionWrapper, LanguageList, LanguageListItem, ListItem, LanguagesSpan }
