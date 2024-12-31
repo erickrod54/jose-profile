99cd58319b0d9f9c9434d9f1ddf9735ef55aa189
Erick Rodriguez <erickrod54@gmail.com>
2024-12-30 21:34:00 -0500

    Jose-profile - version 4.02 - HeaderComponent - Features: Migrating HeaderLogo styles

--- src/components/header.component.jsx
+++ src/components/header.component.jsx
@@ -1,22 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 import { useJoseProfileContext } from "../context";
-import { HeaderContainer, HeaderTitle, HeaderWrapper } from "../style.components";
+import { HeaderContainer, HeaderLogo, HeaderTitle, HeaderWrapper } from "../style.components";
 
-/**Jose-profile web - version 4.01 - HeaderComponent - Features:
+/**Jose-profile web - version 4.02 - HeaderComponent - Features:
  * 
- *      --> writting 'HeaderLogo' styles.
+ *      --> Migrating 'HeaderLogo' styles.
  * 
  * Notes: All these are clean files in data, directory and 
  * files
 */
 
-const HeaderLogo = styled.p`
-    color: hsla(22, 98%, 60%, 1);
-    font-weight: 700;
-    text-transform: capitalize;
-    font-size: calc( 0.50rem + 1vw);
-`
 const HeaderComponent = () => {
 
     const { headerData } = useJoseProfileContext();
