673f3e41710478af0c9d3d54493a8e8302eb2e8b
Erick Rodriguez <erickrod54@gmail.com>
2025-01-02 21:33:00 -0500

    Jose-profile web - version 4.10 - ResumeComponent - featyures: writting ResumeContainer styles

--- src/components/resume.component.jsx
+++ src/components/resume.component.jsx
@@ -1,25 +1,31 @@
 import React from "react";
 import { IntroComponent, WorkComponent, SkillsComponent, LanguagesComponent } from './index.components'
+import styled from "styled-components";
 
-/**Jose-profile web - version 2.02 - ResumeComponent - Features:
+/**Jose-profile web - version 4.10 - ResumeComponent - Features:
  * 
- *      --> Building and placing Components for Resume.
- * 
- *      --> Refactoring the app.
+ *      --> writting 'ResumeContainers'
  * 
  * Notes: All these are clean files in data, directory and 
  * files
 */
 
+const ResumeContainer = styled.section`
+    margin: 1rem;
+    padding: 1rem;
+    display: grid;
+    grid-column: container-start / container-end;
+`
+
 const ResumeComponent = () => {
 
     return(
-        <section id='resume'>
+        <ResumeContainer>
             <IntroComponent />
             <WorkComponent />
             <SkillsComponent />
             <LanguagesComponent /> 
-        </section>
+        </ResumeContainer>
     )
 }
 
