c63e45d607a9673cb2894a6bcdcc51f75415d1c6
Erick Rodriguez <erickrod54@gmail.com>
2025-01-03 21:32:00 -0500

    Jose-profile web - version 4.12 - WorkComponent - Features: writting WorkExperienceWrapper

--- src/components/work.component.jsx
+++ src/components/work.component.jsx
@@ -3,14 +3,33 @@ import styled from "styled-components";
 import { useJoseProfileContext } from "../context";
 
 
-/**Jose-profile web - version 2.16 - WorkComponent - Features:
+/**Jose-profile web - version 4.12 - WorkComponent - Features:
  * 
- *      --> Wrapping 'WorkComponent' with style 
- *         'WorkExperienceWrapper'.
+ *      --> writting 'WorkExperienceWrapper'
  * 
  * Notes: All these are clean files in data, directory and 
  * files
- */
+*/
+
+const WorkExperienceWrapper = styled.section`
+    display: grid;
+    background: white;
+    grid-column: col-start 8 / col-end 6;
+    grid-template-rows: repeat(2);
+
+    section#work-experience > article > section > div > p > span{
+        text-transform: capitalize;
+        font-weight: 700;  
+    }
+    
+    section#work-experience:hover{
+        margin-top: 6rem;
+    }
+    
+    .work-experience--para{
+        margin: 1.7rem;
+    }
+`
 
 const WorkComponent = () => {
     
@@ -18,7 +37,7 @@ const WorkComponent = () => {
     console.log('workExperience data ==>', workExperience);
 
     return(
-        <WorkExperienceWrapper id="work-experience">
+        <WorkExperienceWrapper>
             {workExperience.map((rol) => {
                 const { id, roltitle, company, experience1, experience2, experience3, year } = rol;
                 
@@ -45,20 +64,5 @@ const WorkComponent = () => {
     )
 }
 
-const WorkExperienceWrapper = styled.section`
-
-    section#work-experience > article > section > div > p > span{
-        text-transform: capitalize;
-        font-weight: 700;  
-    }
-
-    section#work-experience:hover{
-      margin-top: 6rem;
-    }
-
-    .work-experience--para{
-       margin: 1.7rem;
-    }
-`
 
 export default WorkComponent;
