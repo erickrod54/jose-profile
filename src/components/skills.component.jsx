bb4dcd6ce442293d0251c9cf83f6b3db2c8fa4dd
Erick Rodriguez <erickrod54@gmail.com>
2025-01-03 21:34:00 -0500

    Jose-profile web - version 4.13 - Skills Component - Features: writting SkillsTitle styles

--- src/components/skills.component.jsx
+++ src/components/skills.component.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { useJoseProfileContext } from "../context";
 import styled from "styled-components";
 
-/**Jose-profile web - version 4.12 - Skills Component - Features:
+/**Jose-profile web - version 4.13 - Skills Component - Features:
  * 
- *      --> writting 'SkillsContainer' styles
+ *      --> writting 'SkillsTitle' styles
  * 
  * Notes: All these are clean files in data, directory and 
  * files
@@ -17,6 +17,18 @@ const SkillsContainer = styled.section`
     grid-column: col-start 4 / col-end 6;        
 `
 
+const SkillsTitle = styled.h2`
+    margin: 1rem;
+    padding: 1rem;
+    color: hsla(22, 98%, 60%, 1);
+    font-weight: 700;
+    text-transform: capitalize;
+    font-size: calc( 0.50rem + 1vw);
+    box-shadow: 0px 2px 0px 0 5px 15px rgba(0, 0, 0, 0.2); /* add box shadow */
+    text-decoration: underline 4px solid #222; /* add underline */
+    text-underline-offset: 4px; /* adjust the distance of the underline from the text */
+`
+
 const SkillsComponent = () => {
 
     const { skills } = useJoseProfileContext();
@@ -26,7 +38,7 @@ const SkillsComponent = () => {
 
     return(
         <SkillsContainer>
-            <h2 className="skills--title">{skillstitle}</h2>
+            <SkillsTitle>{skillstitle}</SkillsTitle>
             {skills.map((skill) => {
                 const { id, skillname  } = skill;
                 return(
