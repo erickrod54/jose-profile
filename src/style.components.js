4d2525178f036a3a7de28891fa8776fcb8c1281c
Erick Rodriguez <erickrod54@gmail.com>
2025-01-04 21:35:00 -0500

    Jose-profile web - version 4.19 - style.components - Features: writting SkillsTitle styles

--- src/style.components.js
+++ src/style.components.js
@@ -1,12 +1,24 @@
 import styled from "styled-components"
 
-/**Jose-profile web - version 4.18 - style.components - Features:
+/**Jose-profile web - version 4.19 - style.components - Features:
  * 
- *      --> writting 'SkillsContainer' styles
+ *      --> writting 'SkillsTitle' styles
  * 
  * Notes: All these are clean files in data, directory and 
  * files
  */
+
+export const SkillsTitle = styled.h2`
+    margin: 1rem;
+    padding: 1rem;
+    color: hsla(22, 98%, 60%, 1);
+    font-weight: 700;
+    text-transform: capitalize;
+    font-size: calc( 0.50rem + 1vw);
+    box-shadow: 0px 2px 0px 0 5px 15px rgba(0, 0, 0, 0.2); /* add box shadow */
+    text-decoration: underline 4px solid #222; /* add underline */
+    text-underline-offset: 4px; /* adjust the distance of the underline from the text */
+`
 
 export const SkillsContainer = styled.section`
     display: grid;
