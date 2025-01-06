1dc7fbe76038fe27e27cf4ea1cf52ae0f87e20fe
Erick Rodriguez <erickrod54@gmail.com>
2025-01-05 21:31:00 -0500

    Jose-profile web - version 4.19 - Skills Component - Features: writting SkillsTitle styles

--- src/components/skills.component.jsx
+++ src/components/skills.component.jsx
@@ -1,28 +1,16 @@
 import React from "react";
 import { useJoseProfileContext } from "../context";
 import styled from "styled-components";
-import { SkillsContainer } from "../style.components";
+import { SkillsContainer, SkillsTitle } from "../style.components";
 
-/**Jose-profile web - version 4.18 - Skills Component - Features:
+/**Jose-profile web - version 4.19 - Skills Component - Features:
  * 
- *      --> writting 'SkillsContainer' styles
+ *      --> writting 'SkillsTitle' styles
  * 
  * Notes: All these are clean files in data, directory and 
  * files
 */
 
-
-const SkillsTitle = styled.h2`
-    margin: 1rem;
-    padding: 1rem;
-    color: hsla(22, 98%, 60%, 1);
-    font-weight: 700;
-    text-transform: capitalize;
-    font-size: calc( 0.50rem + 1vw);
-    box-shadow: 0px 2px 0px 0 5px 15px rgba(0, 0, 0, 0.2); /* add box shadow */
-    text-decoration: underline 4px solid #222; /* add underline */
-    text-underline-offset: 4px; /* adjust the distance of the underline from the text */
-`
 
 const SkillsList = styled.ul`
     margin: 1rem;
