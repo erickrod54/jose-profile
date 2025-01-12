dbd87b96c7230dbab427421b085b8f99df9acc5f
Erick Rodriguez <erickrod54@gmail.com>
2025-01-11 21:31:00 -0500

    Jose-profile - version 5.16 - Workcomponent - features: Migrating CompanyTitle

--- src/components/work.component.jsx
+++ src/components/work.component.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 import { useJoseProfileContext } from "../context";
-import { RolTitleContainer, WorkExperienceContainer, WorkExperienceDescription, WorkExperienceWrapper } from "../style.components";
+import { CompanyTitle, RolTitleContainer, WorkExperienceContainer, WorkExperienceDescription, WorkExperienceWrapper } from "../style.components";
 
 
-/**Jose-profile web - version 5.15 - WorkComponent - Features:
+/**Jose-profile web - version 5.16 - WorkComponent - Features:
  * 
- *      --> Migrating  'RolTitle'
+ *      --> Migrating  'CompanyTitle'
  * 
  * Notes: error:
  * 
  *  Line 22:29:  'RolTitle' is not defined  no-undef
- *  Line 27:27:  'RolTitle' is not defined  no-undef
  *  Line 52:30:  'RolTitle' is not defined 
  * 
  * while dependant components are migrated
@@ -22,11 +21,6 @@ const WorkExperienceContent = styled.section`
     font-size: calc(.7rem + 0.637vw);
     line-height: 2.5;
     margin: 1.7rem;
-`
-
-const CompanyTitle = styled(RolTitle)`
-    font-size: calc( 0.50rem + 0.6vw);
-    color: hsla(220, 98%, 30%, 1);
 `
 
 const TimePeriod = styled(RolTitle)`
