2ac3d3820b9b3aaa00c895861cf99aec5aea9d06
Erick Rodriguez <erickrod54@gmail.com>
2025-01-13 21:33:00 -0500

    Jose-profile web - version 6.02 - style.components - Features: writting HomePageContainer styles

--- src/style.components.js
+++ src/style.components.js
@@ -1,12 +1,37 @@
 import styled from "styled-components"
 
-/**Jose-profile web - version 6.01 - style.components - Features:
+/**Jose-profile web - version 6.02 - style.components - Features:
  * 
- *      --> Refactoring styles for responsiveness by media query
+ *      --> wirtting 'HomePageContainer' styles
  * 
  * Notes: All these are clean files in data, directory and 
  * files
  */
+
+export const HomePageContainer = styled.div`
+    display: grid;
+    grid-template-columns: 
+        [container-start] 1fr 
+        [center-start] repeat(8, [col-start] minmax(min-content, 11rem) [col-end]) 
+        [center-end] 1fr [container-end];
+    grid-template-rows: 30vh repeat(3, min-content);
+    min-height: 100vh;
+    max-width: 100vw;
+    padding: 1rem;
+    gap: 1rem;
+
+    /* Tablet (iPad and below) */
+    @media (max-width: 1024px) {
+        grid-template-columns: [container-start] 1fr [container-end]; /* Single column layout */
+        grid-template-rows: auto;
+        padding: 1rem;
+    }
+
+    /* Mobile (Phones) */
+    @media (max-width: 768px) {
+        padding: 0.5rem;
+    }
+`;
 
 export const ExperienceItem = styled.p`
 
