d2ea50c5acf96a086083cde46089991b2738c685
Erick Rodriguez <erickrod54@gmail.com>
2025-01-14 21:31:00 -0500

    Jose-profile web - version 6.02 - HomePage - Features: Migrating HomePageContainer

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -1,39 +1,15 @@
 import React from "react";
 import { HeaderComponent, ResumeComponent, FooterComponent } from '../components/index.components';
 import styled from "styled-components";
+import { HomePageContainer } from "../style.components";
 
 /**Jose-profile web - version 6.02 - HomePage - Features:
  * 
- *      --> writting 'HomePageContainer'.
+ *      --> Migrating 'HomePageContainer'.
  * 
  * Notes: All these are clean files in data, directory and 
  * files
 */
-
-const HomePageContainer = styled.div`
-    display: grid;
-    grid-template-columns: 
-        [container-start] 1fr 
-        [center-start] repeat(8, [col-start] minmax(min-content, 11rem) [col-end]) 
-        [center-end] 1fr [container-end];
-    grid-template-rows: 30vh repeat(3, min-content);
-    min-height: 100vh;
-    max-width: 100vw;
-    padding: 1rem;
-    gap: 1rem;
-
-    /* Tablet (iPad and below) */
-    @media (max-width: 1024px) {
-        grid-template-columns: [container-start] 1fr [container-end]; /* Single column layout */
-        grid-template-rows: auto;
-        padding: 1rem;
-    }
-
-    /* Mobile (Phones) */
-    @media (max-width: 768px) {
-        padding: 0.5rem;
-    }
-`;
 
 const HomePage = () => {
 
